108e7346fb18acfa6ca2be0620fbc2953e6d393b
Navod D Rajapaksha <129612778+NavodRajapaksha@users.noreply.github.com>
2026-04-22 13:21:51 +0530

    chore: initial project setup with Spring Boot

---
+++ src/main/java/edu/test/LotteryResultManager/LotteryResultManagerApplication.java
@@ -0,0 +1,13 @@
+package edu.test.LotteryResultManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryResultManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryResultManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/test/LotteryResultManager/LotteryResultManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.test.LotteryResultManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryResultManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
